551d4622b7453b50b4fc60a5c3f01a8f297a193d
lhohenrique <lho.henrique@gmail.com>
2018-12-06 22:02:55 -0300

    Criando estrutura básica do projeto

---
+++ PedidoFacil/PedidoFacil/PedidoFacil/Controls/ExpandableView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PedidoFacil.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ExpandableView : ContentView
+	{
+        private TapGestureRecognizer _tapRecogniser;
+
+        private StackLayout _Summary;
+        public virtual StackLayout Summary
+        {
+            get { return _Summary; }
+            set
+            {
+                _Summary = value;
+                SummaryRegion.Children.Add(_Summary);
+                OnPropertyChanged();
+            }
+        }
+
+        private StackLayout _Details;
+        public virtual StackLayout Details
+        {
+            get { return _Details; }
+            set
+            {
+                _Details = value;
+                DetailsRegion.Children.Add(_Details);
+                OnPropertyChanged();
+            }
+        }
+
+        public ExpandableView()
+        {
+            InitializeComponent();
+            InitializeGuestureRecognizer();
+            SubscribeToGuestureHandler();
+
+            DetailsRegion.IsVisible = false;
+        }
+
+        private void InitializeGuestureRecognizer()
+        {
+            _tapRecogniser = new TapGestureRecognizer();
+            SummaryRegion.GestureRecognizers.Add(_tapRecogniser);
+        }
+
+        private void SubscribeToGuestureHandler()
+        {
+            _tapRecogniser.Tapped += TapRecogniser_Tapped;
+        }
+
+        private void TapRecogniser_Tapped(object sender, EventArgs e)
+        {
+            if (DetailsRegion.IsVisible)
+            {
+                DetailsRegion.IsVisible = false;
+            }
+            else
+            {
+                DetailsRegion.IsVisible = true;
+            }
+        }
+    }
+}
---
+++ PedidoFacil/PedidoFacil/PedidoFacil/Services/MenuItemService.cs
@@ -0,0 +1,29 @@
+﻿using PedidoFacil.Models;
+using PedidoFacil.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PedidoFacil.Services
+{
+    public class MenuItemService
+    {
+        private static ObservableCollection<MenuItem> menuList { get; set; }
+
+        public static ObservableCollection<MenuItem> GetMenuItems()
+        {
+            menuList = new ObservableCollection<MenuItem>();
+
+            var pedidos = new MenuItem() { Title = "Pedidos", Icon = "", TargetType = typeof(PedidosPage) };
+            var clientes = new MenuItem() { Title = "Clientes", Icon = "", TargetType = typeof(ClientesPage) };
+            var representadas = new MenuItem() { Title = "Representadas", Icon = "", TargetType = typeof(RepresentadasPage) };
+
+            menuList.Add(pedidos);
+            menuList.Add(clientes);
+            menuList.Add(representadas);
+
+            return menuList;
+        }
+    }
+}
